e06dc3c24e67f5747ec0d83f436d2458ee312a19
iseeqr <iseeqr@gmail.com>
2020-06-22 01:08:57 +0200

    retoques de las imagenes en carta02

--- js/init.js
+++ js/init.js
@@ -14,7 +14,3 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Modal.init(elems);
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.materialboxed');
-  var instances = M.Materialbox.init(elems);
-});
